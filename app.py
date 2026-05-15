f77b708eb55522ca025515cdd3f453c03b93aec0
Andres Gomez Medrano <andres64522278@gmail.com>
2026-05-15 14:37:19 -0500

    Temporary admin

--- app.py
+++ app.py
@@ -404,8 +404,8 @@ def registro():
         try:
             hash_pw = bcrypt.hashpw(contrasena.encode(), bcrypt.gensalt()).decode()
             if correo.lower().endswith("@poligran.edu.co"):
-                from database import Admin
-                nuevo = Admin(nombre=nombre, correo=correo, contrasena=hash_pw)
+                from database import Institucional
+                nuevo = Institucional(nombre=nombre, correo=correo, contrasena=hash_pw)
             else:
                 nuevo = PersonaExterna(nombre=nombre, correo=correo, contrasena=hash_pw)
             db.add(nuevo)
